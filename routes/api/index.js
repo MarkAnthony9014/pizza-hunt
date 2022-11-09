bcd1dba88ad0c5652e0660f90f74c70f2fca88f3
MarkAnthony9014 <markanthony9014@gmail.com>
2022-11-09 14:00:05 -0600

    added comment model

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,8 @@
-// this file will import all of the API routes to prefix their endpoint names and package them up
-
 const router = require('express').Router();
+const commentRoutes = require('./comment-routes');
 const pizzaRoutes = require('./pizza-routes');
 
-// add prefix of `/pizzas` to routes created in `pizza-routes.js`
+router.use('/comments', commentRoutes);
 router.use('/pizzas', pizzaRoutes);
 
 module.exports = router;
